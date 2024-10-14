e919c7a8617d17700fb501c1db55443a4a31cc2f
Niki <Niki@DESKTOP-SNN3CPV>
2024-10-14 22:01:42 +0300

    Add project files.

---
+++ CarsSorter/Cars.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsSorter
+{
+    public class Cars
+    {
+        
+        private string carBrand;
+        private string carModel;
+        private double carEngineSize;
+        
+        public string CarBrand
+        {
+            get { return carBrand; }
+            set { carBrand = value; }
+        }
+        public string CarModel
+        {
+            get { return carModel; }
+            set { carModel = value; }
+
+        }
+        public double CarEngineSize
+        {
+            get { return carEngineSize; }
+            set { carEngineSize = value; }
+        }
+        
+        public Cars(string carBrand, string carModel, double carEngineSize)
+        {
+            CarBrand = carBrand;
+            CarModel = carModel;
+            CarEngineSize = carEngineSize;
+        }
+
+
+
+    }
+}
---
+++ CarsSorter/Program.cs
@@ -0,0 +1,24 @@
+﻿using CarsSorter;
+
+int n = int.Parse(Console.ReadLine());
+List<Cars> cars = new List<Cars>();
+
+for (int i = 0; i < n; i++)
+{
+    string data = Console.ReadLine();
+    string[] dataParts = data.Split();
+
+    string carBrand = dataParts[0];
+    string carModel = dataParts[1];
+    double carEngineSize = double.Parse(dataParts[2]);
+
+    Cars car = new Cars(carBrand, carModel, carEngineSize);
+    cars.Add(car);
+}
+
+cars = cars.OrderByDescending(s => s.CarEngineSize).ToList();
+
+foreach (Cars car in cars)
+{
+    Console.WriteLine($"{car.CarBrand} {car.CarModel}: {car.CarEngineSize}");
+}
